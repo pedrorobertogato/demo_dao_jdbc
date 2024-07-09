86cadc01fea010dddcc540f5a256e3b89f6ad54e
Pedro <pedrortgato@gmail.com>
2024-07-08 21:27:46 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerDao() { //a classe vai expor o método que retorna o tipo da interface mas internamente instancia uma implementação, deixa apenas a interface
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,26 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	private Connection conn; //vai ter ele a disposição na classe para implementação
+	
+	public SellerDaoJDBC(Connection  conn) { //aqui gerou uma dependia do DAO com o banco de dados
+		this.conn = conn;
+	}
+	
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +41,38 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			st.setInt(1, id); //o primeiro elemento vai retornar no id
+			rs = st.executeQuery();
+			if (rs.next()) { //o if vai testar se veio algum resultado
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId")); //aqui ele vai procurar no banco de dados o DepartmentId que retorna o numero 1
+				dep.setName(rs.getString("DepName")); //aqui ele vai procurar no banco de dados o DepartmentId que retorna o nome do departamento
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
