e5ce92f44617a0e3d106a23b521604b4f7a8774b
Pedro <pedrortgato@gmail.com>
2024-07-09 20:57:25 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -91,6 +94,48 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name ");
+			st.setInt(1, department.getId()); //vai buscar o Id no departament
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>(); 
+			Map<Integer, Department> map = new HashMap<>();  //o map vai mapear a lista dentro do while para que não seja criado outro department , o construtor aqui sera vazio para que o getInt Department entre nesse campó
+			
+			while (rs.next()) { //o while vai ser enquanto na leitura existir durtante a busca pelo vendedor
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); //vai verificar se o departament não existe antes de gerar um novo, sendo assim ele vai buscar dentro de DepartmentID
+				
+				if (dep == null) { //caso não encontra o vendedor no departmentId que será vai instanciar o departamento como retorno
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); //vai salvar o departamento dentro do map para a próxima vez que for verificar ver que ele já existe
+				}
+				
+				Seller obj = instatiateSeller(rs, dep);
+				list.add(obj); //no final ele vai acrescentar o vendedor a lista
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
